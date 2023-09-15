087a2a43bd16b0d93279fae44cc162c9d25a7324
patriotkusuma <patriot.kusumasejati@gmail.com>
2023-09-15 10:01:56 +0700

    add production env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application(
 |
 */
 $env = $app->detectEnvironment(function() {
-    return getenv('APP_ENV') ? 'production': 'local';
+    return getenv('APP_ENV') ?: 'local';
 
 });
 
